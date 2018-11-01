807dd43adf85865b42f39a0e93e870237c1d121c
Çağıl Uluşahin <cagilulusahin@gmail.com>
2018-11-01 20:43:35 +0000

    find method

---
+++ find_correct_pattern.py
@@ -0,0 +1,24 @@
+import re
+
+import wikipedia
+
+
+def find_question_body(title):
+    summary = wikipedia.summary(title, sentences=1)
+    m = re.match(f"{title}.*?is a (.*$)", summary)
+    if m:
+        return m.groups()[0]
+    else:
+        return False
+
+
+"""
+If it can find
+find_question_body('Albert Einstein')
+--> 'German-born theoretical physicist who developed the theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics).'
+
+If cannot match
+find_question_body('Albert Einstein')
+--> False
+
+"""
